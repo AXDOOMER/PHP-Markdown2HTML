3af9e6628ecce96c10a8802f1f2212a833ec193e
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2019-01-06 16:14:27 -0500

    Initial project that works

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<body>
+
+<?php
+	$dir = '.';
+	$filesarr = scandir($dir);
+
+	foreach ($filesarr as $value) {
+		// Check extension to hide other files
+		$ext = pathinfo($value, PATHINFO_EXTENSION);
+		if ($ext == "md") {
+			echo("<a href=\"index.php?page=$value\">$value</a><br/>");
+		}
+	}
+?>
+
+<hr/>
+
+<?php
+	include 'Parsedown.php';
+
+	if (isset($_GET["page"]))
+	{
+		// Check extension to prevent loading other files
+		$ext = pathinfo($_GET["page"], PATHINFO_EXTENSION);
+		if ($ext == "md") {
+			$Parsedown = new Parsedown();
+
+			$myfile = fopen($_GET["page"], "r") or die("Unable to open page");
+			$content = fread($myfile, filesize($_GET["page"]));
+			fclose($myfile);
+
+			echo($Parsedown->text($content));
+		}
+	} else {
+		echo("Hi! Please select a link to visit.");
+	}
+?>
+
+</body>
+</html>
